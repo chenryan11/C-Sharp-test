c442272bf5ee1460c96341d83d47c34c5ee2ea5b
Ryan <chenryan11@yahoo.com.tw>
2023-07-11 06:16:00 +0800

    ADD 讀檔跟寫檔流程

---
+++ SaveAndLoadFile/SaveAndLoadFile/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace SaveAndLoadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            loadFile();
+
+            saveFile();
+        }
+
+        public static void loadFile()
+        {
+            String line;
+
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("D:\\vs\\c#\\SaveAndLoadFile\\SaveAndLoadFile\\test.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        public static void saveFile()
+        {
+            string path = "D:\\vs\\c#\\SaveAndLoadFile\\SaveAndLoadFile\\file.txt";
+            string Text = "Hello and Welcome";
+            using (StreamWriter writetext = new StreamWriter(path))
+            {
+                writetext.WriteLine(Text);
+            }
+
+           /* try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("C:\\Test.txt");
+                //Write a line of text
+                sw.WriteLine("Hello World!!");
+                //Write a second line of text
+                sw.WriteLine("From the StreamWriter class");
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }*/
+        }
+    }
+}
